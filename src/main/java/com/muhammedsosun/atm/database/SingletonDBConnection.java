80e8e8967028cb61cc0f5225d1a92ba57865162c
Muhammed <muhammedsosun06@gmail.com>
2025-03-22 17:42:15 +0300

    SingletonDBConnection

--- src/main/java/com/muhammedsosun/atm/database/SingletonDBConnection.java
+++ src/main/java/com/muhammedsosun/atm/database/SingletonDBConnection.java
@@ -9,26 +9,49 @@ import java.sql.SQLException;
 
 public class SingletonDBConnection {
 
-    private static final String URL = "jdbc:h2:./h2db/blog" + "AUTO_SERVER=TRUE";
+    // private static final String URL = "jdbc:h2:./h2db/blog" + "AUTO_SERVER=TRUE";
+    private static final String URL = "jdbc:h2:./h2db/user_management";
     private static final String USERNAME ="sa";
     private static final String PASSWORD="";
 
+    private static SingletonDBConnection instance;
+    private  Connection connection;
+
     private SingletonDBConnection(){
-
+        try {
+            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            System.out.println(SpecialColor.GREEN + "Veritabanı bağlantısı başarılı" + SpecialColor.RESET);
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println(SpecialColor.GREEN + "Veritabanı bağlantısı başarısız" + SpecialColor.RESET);
+            throw new RuntimeException("Veritabanı bağlantısı başarısız");
+        }
     }
-    private static Connection connection;
 
-    public static Connection getConnection() throws SQLException {
-        if (connection == null|| connection.isClosed()){
+    // Singleton Design Intance
+    public static synchronized SingletonDBConnection getInstance(){
+        if(instance==null){
+            instance= new SingletonDBConnection();
+        }
+        return instance;
+    }
+
+    public  Connection getConnection(){
+        return connection;
+    }
+    public static void closeConnection(){
+        if (instance!= null && instance.connection != null){
             try {
-                connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
-                System.out.println(SpecialColor.GREEN + "Veritabanı bağlantısı başarılı" + SpecialColor.RESET);
-            }catch (SQLException sqlException){
-                sqlException.printStackTrace();
-                System.out.println(SpecialColor.GREEN + "Veritabanı bağlantısı başarısız" + SpecialColor.RESET);
-                throw new RuntimeException("Veritabanı bağlantısı başarısız");
+                instance.connection.close();
+                System.out.println(SpecialColor.RED+ "Veritabanı bağlantısı kapatıldı"+ SpecialColor.RESET);
+
+            }catch (SQLException e){
+                throw new RuntimeException(e);
             }
         }
-        return connection;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
